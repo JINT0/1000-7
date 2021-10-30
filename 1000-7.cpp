c6fb721d0a419ed30924dacdac400333b5310667
JINT <93275045+JINT0@users.noreply.github.com>
2021-10-31 00:56:05 +0300

    Add files via upload

---
+++ 1000-7.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "windows.h" // Library
+using namespace std;
+
+int main() {
+    int start_sum, sum = 1000;
+
+    while (sum > 7)
+{
+        start_sum = i;
+        sum = sum - 7;
+        cout << start_sum << " - 7" << " = " << i << endl;
+
+        Sleep(15);
+}
+    cout << "I`m a ghoul";
+    cin.get();
+    return 0;
+}
+ 
